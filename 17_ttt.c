0890889e7468b3c25708490c14cd901fc8aed304
Sanyam Jain <137413162+sxnyum@users.noreply.github.com>
2025-01-10 02:10:53 +0530

    17_ttt.c

--- 17_ttt.c
+++ 17_ttt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char empty[10] = {'o', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 int choice, player;
 
